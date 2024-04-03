71eb65235a45cd8d2e22505cf03135493f9f6da2
Sabbir Ahmed <sabbir.ahmed@bertelsmann.de>
2024-04-03 15:49:12 +0800

    Init commit.

---
+++ src/assets/js/app.ts
@@ -0,0 +1,214 @@
+import confetti from 'canvas-confetti';
+import Slot from '@js/Slot';
+import prizes from '../../PrizeList.json';
+import SoundEffects from '@js/SoundEffects';
+import fs from 'fs';
+
+// Initialize slot machine
+(() => {
+  let totalPrize = 50;
+  let prizeIndex = 0;
+
+  //const fs = require("fs/promises");
+
+  const drawButton = document.getElementById('draw-button') as HTMLButtonElement | null;
+  const confirmButton = document.getElementById('confirm-button') as HTMLButtonElement | null;
+  const fullscreenButton = document.getElementById('fullscreen-button') as HTMLButtonElement | null;
+  const settingsButton = document.getElementById('settings-button') as HTMLButtonElement | null;
+  const settingsWrapper = document.getElementById('settings') as HTMLDivElement | null;
+  const settingsContent = document.getElementById('settings-panel') as HTMLDivElement | null;
+  const settingsSaveButton = document.getElementById('settings-save') as HTMLButtonElement | null;
+  const settingsCloseButton = document.getElementById('settings-close') as HTMLButtonElement | null;
+  const sunburstSvg = document.getElementById('sunburst') as HTMLImageElement | null;
+  const confettiCanvas = document.getElementById('confetti-canvas') as HTMLCanvasElement | null;
+  const nameListTextArea = document.getElementById('name-list') as HTMLTextAreaElement | null;
+  const prizeNameParagraph = document.getElementById('prize') as HTMLElement | null;
+  const removeNameFromListCheckbox = document.getElementById('remove-from-list') as HTMLInputElement | null;
+  const enableSoundCheckbox = document.getElementById('enable-sound') as HTMLInputElement | null;
+
+  // Graceful exit if necessary elements are not found
+  if (!(
+    drawButton
+    && confirmButton
+    && fullscreenButton
+    && settingsButton
+    && settingsWrapper
+    && settingsContent
+    && settingsSaveButton
+    && settingsCloseButton
+    && sunburstSvg
+    && confettiCanvas
+    && nameListTextArea
+    && prizeNameParagraph
+    && removeNameFromListCheckbox
+    && enableSoundCheckbox
+  )) {
+    console.error('One or more Element ID is invalid. This is possibly a bug.');
+    return;
+  }
+
+  if (!(confettiCanvas instanceof HTMLCanvasElement)) {
+    console.error('Confetti canvas is not an instance of Canvas. This is possibly a bug.');
+    return;
+  }
+
+  const soundEffects = new SoundEffects();
+  const MAX_REEL_ITEMS = 40;
+  const CONFETTI_COLORS = ['#26ccff', '#a25afd', '#ff5e7e', '#88ff5a', '#fcff42', '#ffa62d', '#ff36ff'];
+  let confettiAnimationId;
+
+  //First time Prize Name set
+  if (prizeNameParagraph instanceof HTMLElement) {
+    prizeNameParagraph.innerHTML = '<div>' + (totalPrize - prizeIndex) + ". " + prizes[prizeIndex] + '</div>';
+  }
+  confirmButton.disabled = true;
+
+  /** Confeetti animation instance */
+  const customConfetti = confetti.create(confettiCanvas, {
+    resize: true,
+    useWorker: true
+  });
+
+  //Prize Name show
+  const showPrizeName = () => {
+    if (prizeNameParagraph instanceof HTMLElement) {
+      prizeNameParagraph.innerHTML = '<div>' + (totalPrize - prizeIndex) + ". " + prizes[prizeIndex] + '</div>';
+    }
+  }
+
+  const savePersonPrize = () => {
+    let prizeName = prizes[prizeIndex];
+    let personName = slot.currentWinnerName;
+
+    console.log("###### Prize Name: " + prizeName);
+    console.log("###### Winner Name: " + personName);
+
+    const data = fs.readFileSync("../../PersonPrizeList.json", 'utf-8');
+    console.log("Data: " + data);
+  }
+  
+  /** Triggers cconfeetti animation until animation is canceled */
+  const confettiAnimation = () => {
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+    const confettiScale = Math.max(0.5, Math.min(1, windowWidth / 1100));
+
+    customConfetti({
+      particleCount: 1,
+      gravity: 0.8,
+      spread: 90,
+      origin: { y: 0.6 },
+      colors: [CONFETTI_COLORS[Math.floor(Math.random() * CONFETTI_COLORS.length)]],
+      scalar: confettiScale
+    });
+
+    confettiAnimationId = window.requestAnimationFrame(confettiAnimation);
+  };
+
+  /** Function to stop the winning animation */
+  const stopWinningAnimation = () => {
+    if (confettiAnimationId) {
+      window.cancelAnimationFrame(confettiAnimationId);
+    }
+    sunburstSvg.style.display = 'none';
+  };
+
+  /**  Function to be trigger before spinning */
+  const onSpinStart = () => {
+    stopWinningAnimation();
+    drawButton.disabled = true;
+    settingsButton.disabled = true;
+    soundEffects.spin((MAX_REEL_ITEMS - 1) / 10);
+  };
+
+  /**  Functions to be trigger after spinning */
+  const onSpinEnd = async () => {
+    confettiAnimation();
+    sunburstSvg.style.display = 'block';
+    await soundEffects.win();
+    confirmButton.disabled = false;
+    drawButton.disabled = false;
+    settingsButton.disabled = false;
+  };
+
+  /** Slot instance */
+  const slot = new Slot({
+    reelContainerSelector: '#reel',
+    maxReelItems: MAX_REEL_ITEMS,
+    onSpinStart,
+    onSpinEnd,
+    onNameListChanged: stopWinningAnimation
+  });
+
+  /** To open the setting page */
+  const onSettingsOpen = () => {
+    nameListTextArea.value = slot.names.length ? slot.names.join('\n') : '';
+    removeNameFromListCheckbox.checked = slot.shouldRemoveWinnerFromNameList;
+    enableSoundCheckbox.checked = !soundEffects.mute;
+    settingsWrapper.style.display = 'block';
+  };
+
+  /** To close the setting page */
+  const onSettingsClose = () => {
+    settingsContent.scrollTop = 0;
+    settingsWrapper.style.display = 'none';
+  };
+
+  // Click handler for "Draw" button
+  drawButton.addEventListener('click', () => {
+    if (!slot.names.length) {
+      onSettingsOpen();
+      return;
+    }
+
+    slot.spin();
+  });
+
+  // Click handler for "Confirm" button
+  confirmButton.addEventListener('click', () => {
+    savePersonPrize();
+
+    prizeIndex = prizeIndex + 1;
+    let reelContainer = document.querySelector('#reel') as HTMLElement | null;
+    if (reelContainer instanceof HTMLElement) {
+      reelContainer.removeChild(reelContainer.childNodes[0]);
+    }
+    stopWinningAnimation();
+    showPrizeName();
+    confirmButton.disabled = true;
+  });
+
+  // Hide fullscreen button when it is not supported
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - for older browsers support
+  if (!(document.documentElement.requestFullscreen && document.exitFullscreen)) {
+    fullscreenButton.remove();
+  }
+
+  // Click handler for "Fullscreen" button
+  fullscreenButton.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+      return;
+    }
+
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  });
+
+  // Click handler for "Settings" button
+  settingsButton.addEventListener('click', onSettingsOpen);
+
+  // Click handler for "Save" button for setting page
+  settingsSaveButton.addEventListener('click', () => {
+    slot.names = nameListTextArea.value
+      ? nameListTextArea.value.split(/\n/).filter((name) => Boolean(name.trim()))
+      : [];
+    slot.shouldRemoveWinnerFromNameList = removeNameFromListCheckbox.checked;
+    soundEffects.mute = !enableSoundCheckbox.checked;
+    onSettingsClose();
+  });
+
+  // Click handler for "Discard and close" button for setting page
+  settingsCloseButton.addEventListener('click', onSettingsClose);
+})();
